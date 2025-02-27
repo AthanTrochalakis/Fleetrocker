7fd74ec20318de4bb6af214802a8ddcb8ab31303
MaxKMurata <maxwellkmurata@gmail.com>
2025-02-27 09:28:26 -0800

    Add files via upload
    This is the fourth commit for ControlClass.java

--- ControlClass.java
+++ ControlClass.java
@@ -1,8 +1,7 @@
 import java.util.*;
 public class ControlClass extends JFrame implements KeyListener {
 
-JLabel = label; //creates the label that is moving on the GUI
-
+    
 //JFrame GUI components
 
 MyFrame() {
@@ -12,15 +11,6 @@ this.setSize(,);//set the size of the frame
 this.setLayout(null);
 this.addKeyListener(this);
 this.setVisible(true);//sets the frame visible
-
-
-label = new JLabel();
-label.setBounds(x,y,width,height);//Instantiates the label at x and y, with specified width and height
-label.setBackroundColor();//sets backround color
-label.setOpaque(true);//sets opacity
-
-this.add(label);//add label to GUI
-this.setVisible(true);//Set visible
 
 
 //Creates a new player object
@@ -38,13 +28,13 @@ public void keyTyped(KeyEvent e) {
 
 switch(e.getKeyChar())
     
-case 'a': label.setLocation(.getX()-10, label.getY());
+case 'a': One.setLocation(One.getX()-10, One.getY());
 break;
-case 'w': label.setLoaction(.getX(), label.getY()-10);
+case 'w': One.setLoaction(One.getX(), One.getY()-10);
 break;
-case 's': label.setLocation(.getX(), label.getY()+10);
+case 's': One.setLocation(One.getX(), One.getY()+10);
 break;
-case 'd': label.setLocation(.getX()+10), label.getY()); 
+case 'd': One.setLocation(One.getX()+10), One.getY()); 
 break;
 
 }
