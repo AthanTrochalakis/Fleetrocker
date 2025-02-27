55e90071f190061b9174f50db0ae134a39ef3226
AthanTrochalakis <atrochalakis@pugetsound.edu>
2025-02-27 10:17:15 -0800

    Main method file with control class support

--- Fleetrocker.java
+++ Fleetrocker.java
@@ -5,7 +5,7 @@ import java.io.File;
  * Runs Fleetrocker
  *
  * @author Athan Trochalakis
- * @version 2/26/2025
+ * @version 2/27/2025
  */
 public class Fleetrocker{
     
@@ -16,8 +16,15 @@ public class Fleetrocker{
 	for(int i = 0; i < listOfFiles.length; i++){
 	    levelFiles.add(listOfFiles[i]);
 	}
-	for(int i = 0; i < levelFiles.size(); i++){
-	    System.out.println("Found file: " + levelFiles.get(i).getName());
-	}
+	//for(int i = 0; i < levelFiles.size(); i++){
+	// System.out.println("Found file: " + levelFiles.get(i).getName());
+	//}
+
+	int randomLevel = (int)(Math.random() * levelFiles.size());
+	//System.out.println("Displaying " + levelFiles.get(randomLevel).getName());
+	Level newLevel = new Level("../levelFiles/" + levelFiles.get(randomLevel).getName(), 1);
+
+	ControlClass newControl = new ControlClass();
+	newControl.level(newLevel.getLayout());
     }
 }
